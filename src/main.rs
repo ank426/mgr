7e6742d2bd51b1dc5d2aeca4b953ffba37f91fc5
ankit <ankit.avinash@gmail.com>
2026-03-01 14:17:57 +0530

    init arg parse

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long)]
+    generate: bool,
+
+    #[arg(short, long, default_value_t = 8080)]
+    port: u16,
+
+    #[arg(default_value = ".")]
+    path: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{:?}", args.path);
 }
