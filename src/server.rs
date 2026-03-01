310f67c64f7d71c5b521c7cf098504df6b1ab7d5
ankit <ankit.avinash@gmail.com>
2026-03-01 16:47:00 +0530

    clean

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,3 @@
-use std::convert::Infallible;
 use std::sync::Arc;
 
 use warp::Filter;
@@ -15,8 +14,8 @@ pub async fn serve(manga: Manga, port: u16) {
     let html_route = warp::path::end().map(move || warp::reply::html(html.clone()).into_response());
 
     let pages_for_route = Arc::clone(&pages);
-    let page_route = warp::path!("page" / usize)
-        .and(with_pages(pages_for_route))
+    let page_route = warp::path!("page" / u32)
+        .and(warp::any().map(move || Arc::clone(&pages_for_route)))
         .map(page_response);
 
     let routes = html_route.or(page_route);
@@ -25,14 +24,8 @@ pub async fn serve(manga: Manga, port: u16) {
     warp::serve(routes).run(addr).await;
 }
 
-fn with_pages(
-    pages: Arc<Vec<Page>>,
-) -> impl Filter<Extract = (Arc<Vec<Page>>,), Error = Infallible> + Clone {
-    warp::any().map(move || Arc::clone(&pages))
-}
-
-fn page_response(index: usize, pages: Arc<Vec<Page>>) -> Response<Vec<u8>> {
-    if let Some(page) = pages.get(index) {
+fn page_response(index: u32, pages: Arc<Vec<Page>>) -> Response<Vec<u8>> {
+    if let Some(page) = pages.get(index as usize) {
         return Response::builder()
             .status(StatusCode::OK)
             .header("content-type", page.mime)
@@ -55,39 +48,7 @@ fn build_html(title: &str, count: usize) -> String {
         ));
     }
 
-    format!(
-        "<!doctype html>
-<html lang=\"en\">
-<head>
-  <meta charset=\"utf-8\" />
-  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />
-  <title>{title}</title>
-  <style>
-    :root {{
-      color-scheme: light;
-    }}
-    body {{
-      margin: 0;
-      background: #0e0e0e;
-      color: #f6f6f6;
-      font-family: ui-sans-serif, system-ui, -apple-system, Segoe UI, Roboto, Helvetica, Arial, sans-serif;
-    }}
-    main {{
-      width: 100%;
-    }}
-    img {{
-      display: block;
-      width: 100%;
-      height: auto;
-      margin: 0;
-    }}
-  </style>
-</head>
-<body>
-  <main>
-    {images}
-  </main>
-</body>
-</html>"
-    )
+    include_str!("viewer.html")
+        .replace("{title}", title)
+        .replace("{images}", &images)
 }
