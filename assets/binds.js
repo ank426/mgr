2e3e4a6a99515fee1b1b6d8f785c5058426b38f8
ankit <ankit.avinash@gmail.com>
2026-03-28 13:41:10 +0530

    clean zoom

--- assets/binds.js
+++ assets/binds.js
@@ -6,13 +6,15 @@
 export async function onKey(viewer, event) {
     switch (event.key) {
         case "=":
-            viewer.state.zoom = Math.min(500, viewer.state.zoom + 5);
+        case "-": {
+            if (event.key === "=") viewer.state.zoom += 5;
+            if (event.key === "-") viewer.state.zoom -= 5;
+            viewer.state.zoom = Math.max(10, Math.min(500, viewer.state.zoom));
+            const centerRatio = (scrollX + innerWidth / 2) / document.documentElement.scrollWidth;
             viewer.withScrollRestore(() => (viewer.pagesRoot.style.width = `${viewer.state.zoom}%`));
+            scrollTo(centerRatio * document.documentElement.scrollWidth - innerWidth / 2, scrollY);
             break;
-        case "-":
-            viewer.state.zoom = Math.max(10, viewer.state.zoom - 5);
-            viewer.withScrollRestore(() => (viewer.pagesRoot.style.width = `${viewer.state.zoom}%`));
-            break;
+        }
         case "j":
             event.preventDefault();
             window.scrollTo({ top: window.scrollY + window.innerHeight / 2 });
